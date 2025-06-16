dc41888fb74678b86aeec59f25dca62f15f6a3c4
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-06-16 10:37:40 +0200

    added a debug board for size 25

--- js/sudoku/constants.js
+++ js/sudoku/constants.js
@@ -6,7 +6,8 @@ export const DEBUG_BOARDS = new Map([
   /* eslint-disable @typescript-eslint/no-magic-numbers */
   [4, '?s=2kCWOHm&m=e7W&d=4'],
   [9, '?s=1rNvQlP59hY1UJMFE5DX3hwk1qIBLnXS6ilfDGy4IGdhsC&m=bIF2NvxgrvtuEt&d=9'],
-  [16, '?s=hpbjaN1Y0HMLqNcMAz1H6NmKmOMSDZBpjCBHl8dhKhATBXumBoANHGsbnmOEzTkz6ySCcfcC4LMrWKjtcnj7WF611o0NmqeiqasiTMDrNBWf7kUCpC1Yb6ExeVjZ4TopShgiCbnIhyYaAhkHaUko7rwbO5VpQjDjH8lgDvr8EmRs3VS0&m=UqAbrOMIk6BBuY6VoSasyyUP7hzZqaraIZQyp6q0d2J&d=g']
+  [16, '?s=hpbjaN1Y0HMLqNcMAz1H6NmKmOMSDZBpjCBHl8dhKhATBXumBoANHGsbnmOEzTkz6ySCcfcC4LMrWKjtcnj7WF611o0NmqeiqasiTMDrNBWf7kUCpC1Yb6ExeVjZ4TopShgiCbnIhyYaAhkHaUko7rwbO5VpQjDjH8lgDvr8EmRs3VS0&m=UqAbrOMIk6BBuY6VoSasyyUP7hzZqaraIZQyp6q0d2J&d=g'],
+  [25, '?s=1rCOSfPzQOeudAZ9m8Oy999CxkkEVldIOJFUQEzxCP0r9UpEghirv6argXEaeTgmFYGzgU8w37AXKDnq1IC0OaZKuAvq2ucit93d8QafCLcltCc3M8iLsyB5YfUYPyTZEe4EDhfYkrlHVw5j5Plk4mYBTbKOX4xZuma0ICk8wlKzNbyotO8cPBfgPywdrp1TQtzS57O0argCrwnk6sXZ1kouvQnvDNzZwTQrhbYXNNnSkhLcEAhqKROi73cr1QEG06ZZwY7SyInKwV10zh0O8vxcKrja7V1VKGi6mIClU82DjhSIiXukRMGreXgDv2ghXfmDNqBDlFhbagGCDjnW7WsTK3vIcGuadHO9PN3MYzCdHNohcJZaT0u7VgCuWCMd9ZO5FiRbkNIq3DPnwam2o6KVfCT3VXIPNmMZjQwBr9jvOzeBCfxlcz4mG4hUQJKsFx6fGurD0SWDHO0Wpx42YGO7SkyMQR6uqDLNyuKW7tbUMF&m=r6qJKq3BQDgjsNIGdaTn0gci7SzX3ietXdo8f974O25AjVapd2FbtnCSkyMKM27thA7WQiYgplCd2l534CNK7AOmHyy5Uz0G2ObKhElWX&d=p']
   /* eslint-enable @typescript-eslint/no-magic-numbers */
 ]);
 
