4b6917c3dd486a49c5e953db6eccd2a0f8853560
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-10-20 10:14:44 +0200

    fix `Swal` global definition

--- globals.d.ts
+++ globals.d.ts
@@ -1,8 +1,7 @@
 import type Sweetalert2 from 'sweetalert2';
 
 declare global {
-  // @ts-expect-error must be done this way to work
-  const Swal = Sweetalert2;
+  const Swal: typeof Sweetalert2;
 
   function rando(min: number, max?: number): number;
   function randoSequence<val>(arr: val[]): { index: number; value: val }[];
