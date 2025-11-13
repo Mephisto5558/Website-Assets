233624b42e87375fa0f8060f04cd55d4762e812a
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-11-13 16:14:57 +0100

    fix js build script

--- min/js/vote/createElement.js
+++
@@ -1,2 +0,0 @@
-"use strict";(()=>{function d(f,E,o,a=!1){let e=document.createElement(f);if(E)for(let t of Object.keys(E)){let n=E[t];n!==void 0&&(typeof n=="object"&&n!=null&&!Array.isArray(n)&&t in e?Object.assign(e[t],n):e[t]=n)}return o&&(a?o.replaceChildren(e):o.append(e)),e}})();
-//# sourceMappingURL=createElement.js.map
--- min/js/vote/state.js
+++
@@ -1,2 +0,0 @@
-"use strict";(()=>{var e={cardsCache:new Map,user:void 0,cardsOffset:0,pageIsLoaded:!1,smallScreen:!1},a=e;})();
-//# sourceMappingURL=state.js.map
