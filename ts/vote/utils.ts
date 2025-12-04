4e6e70db61a370ecde2ae98bffa4d7f59a6661aa
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2025-12-04 16:21:14 +0100

    add card creation date

--- ts/vote/utils.ts
+++ ts/vote/utils.ts
@@ -223,9 +223,16 @@ export function createCardElement(card: Card): void {
     createElement('i', { className: 'far fa-trash-can fa-xl' }, deleteButtonElement);
   }
 
+  const metadataContainer = createElement('div', { id: 'metadata-container' }, voteButtonsElement);
+  if (card.id.includes('_')) { // old github project PVTI type
+    /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
+    const date = new Date(card.id.split('_')[1]!);
+    createElement('time', { title: 'Creation Date', dateTime: date.toISOString(), textContent: date.toLocaleDateString('en') }, metadataContainer);
+  }
+
   if (isDev) {
     /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
-    createElement('p', { id: 'userId', title: 'Click to copy', textContent: card.id.split('_')[0]! }, voteButtonsElement)
+    createElement('p', { id: 'userId', title: 'Click to copy', textContent: card.id.split('_')[0]! }, metadataContainer)
     /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
       .addEventListener('click', async () => navigator.clipboard.writeText(card.id.split('_')[0]!));
   }
